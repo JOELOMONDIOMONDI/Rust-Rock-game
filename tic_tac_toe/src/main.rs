2f6c65c00f6a0ac03e37a2d1ef3102a1ffc02f27
JOEL <joelomondi2003@gmail.com>
2023-12-20 22:15:09 +0300

    Implementation to find the winner is fully functional

--- tic_tac_toe/src/main.rs
+++ tic_tac_toe/src/main.rs
@@ -4,40 +4,65 @@ use rand::Rng;
  
 // MY FIRST GAME IN RUST
 fn main() {
-    let choices: [&str; 3]=["Rock", "Paper", "Scissor"];
-    println!("SAMPLE TIC TAC TOE GAME");
-    get_user_choice();
-    get_comp_choice();
-}
-
-// Function to get computer choice
-fn get_comp_choice(){
-    let rand_number=rand::thread_rng().gen_range(1..=3);
-    println!("Computer range is {}",rand_number);
-    let comp: &str="";
-
-}
-// Function to get user choice
-fn get_user_choice(){
-    let mut user_choice: String=String::new();
-    println!("Enter either Rock, paper or scissor");
-
-    io::stdin()
-    .read_line(&mut user_choice)
-    .expect("Erro getting user input");
-
-   println!("Your choice is {}", user_choice)
+     println!("SAMPLE TIC TAC TOE GAME");
+    let us_choice: &str= get_user_choice();
+    let com_choice: &str= get_comp_choice();
+    let get_winner: &str=find_winner(us_choice, com_choice);
+    println!("Winner is {}", get_winner);
  
 }
 
-// Function to get the winner
-fn find_winner(){
+// Function to get computer choice
+fn get_comp_choice() ->& 'static str{
+
+    let choices: [&str; 3]=["Rock", "Paper", "Scissor"];
+    let rand_number: usize=rand::thread_rng().gen_range(1..=3);
+     let comp_choice: &str=choices[rand_number];
+    println!("Computer choice is {}",comp_choice);
+    comp_choice
+
+
+ 
+}
+// Function to get user choice
+fn get_user_choice() ->& 'static str{
+    let mut user_choice=String::new();
+    println!("Enter either Rock, paper or scissor");
+    io::stdin()
+    .read_line(&mut user_choice)
+    .expect("Erro getting user input");
     
+   println!("Your choice is {}", user_choice.trim());
+   
+    match user_choice.trim().to_lowercase().as_str() {
+        "rock" => "Rock",
+        "paper" => "Paper",
+        "scissor" | "scissors" => "Scissor",
+        _ => panic!("Invalid choice"), // Handle invalid input
+    }
+
     
   
 }
 
-// The overal Function
-fn playground(){
+// Function to get the winner
+fn find_winner(user:&str, computer:&str)->& 'static str{
+    
+    if user == computer {
+        "draw"
 
+    }
+    else if (computer == "Paper" && user == "Rock") ||
+            (computer == "Rock" && user == "Scissors") ||
+            (computer == "Scissors" && user == "Paper")
+     {
+        "computer  wins"
+    }
+    else {
+      "user wins"
+   }
+  
 }
+    
+
+ 
