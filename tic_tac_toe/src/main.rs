0402d14a532411bed51772c7e1181bcaaa00caf7
JOEL <joelomondi2003@gmail.com>
2023-12-24 21:13:06 +0300

    Added function to increment and the playground function

--- tic_tac_toe/src/main.rs
+++ tic_tac_toe/src/main.rs
@@ -4,19 +4,32 @@ use rand::Rng;
  
 // MY FIRST GAME IN RUST
 fn main() {
-     println!("SAMPLE TIC TAC TOE GAME");
-    let us_choice: &str= get_user_choice();
-    let com_choice: &str= get_comp_choice();
-    let get_winner: &str=find_winner(us_choice, com_choice);
-    println!("Winner is {}", get_winner);
+    
+      
+    println!("SAMPLE TIC TAC TOE GAME");
+    playground();
+     
  
 }
+
+    //    PLAYGROUND
+    fn playground(){
+      let us_choice: &str= get_user_choice();
+      let com_choice: &str= get_comp_choice();
+      let get_winner: &str=find_winner(us_choice, com_choice);
+      println!("{} The game", get_winner);
+      increment(get_winner);
+ 
+
+
+ 
+     }
 
 // Function to get computer choice
 fn get_comp_choice() ->& 'static str{
 
     let choices: [&str; 3]=["Rock", "Paper", "Scissor"];
-    let rand_number: usize=rand::thread_rng().gen_range(1..=3);
+    let rand_number: usize=rand::thread_rng().gen_range(0..=2);
      let comp_choice: &str=choices[rand_number];
     println!("Computer choice is {}",comp_choice);
     comp_choice
@@ -64,5 +77,28 @@ fn find_winner(user:&str, computer:&str)->& 'static str{
   
 }
     
+// FUNCTION TO INCREMENT THE SCORES
+fn increment(value:&str) {
+   
+     let mut user_score: i32 =0;
+     let mut comp_score: i32 =0;
+     
+
+    if value=="computer  wins"{
+        comp_score +=1;
+        println!("Comp score is {}",comp_score);
+        // comp_score
+        
+    }
+
+    else if value== "user wins"{
+        user_score +=1;
+        println!("User score is {}", user_score);
+        // user_score
+    }
+    else {
+         return
+     }
+}
 
  
